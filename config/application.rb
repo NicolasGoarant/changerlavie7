7dda0a408177e518660e198b465ea82fe33a8d14
nicolas goarant <nicolas.goarant@hotmail.fr>
2020-03-14 14:58:15 +0100

    remplace par a-mon-tour

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 
 
-module Changerlavie7
+module a_mon_tour
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2

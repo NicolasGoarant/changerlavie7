102cf91419eba65a7c824e1c161628725465a52a
nicolas goarant <nicolas.goarant@hotmail.fr>
2020-03-14 12:23:08 +0100

    config

---
+++ config/webpack/environment.js
@@ -0,0 +1,8 @@
+const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery'
+}))
+environment.loaders.delete('nodeModules');
+module.exports = environment

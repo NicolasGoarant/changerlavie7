225d7e999a0453cce0cdc41094fbcfd396dd5647
nicolas goarant <nicolas.goarant@hotmail.fr>
2020-04-15 18:40:33 +0200

    articles invisibles

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -2,6 +2,11 @@ class ArchivesController < ApplicationController
     def show
     @articles = Article.geocoded.order("publication DESC").where(status: "archive")
     @markers = @articles.as_json(only:[:id, :summary, :title, :latitude, :longitude], methods: [:properties])
-   end
+    end
+
+    def index
+    @articles = Article.geocoded.order("publication DESC").where(status: "invisible")
+    @markers = @articles.as_json(only:[:id, :summary, :title, :latitude, :longitude], methods: [:properties])
+    end
 
 end
